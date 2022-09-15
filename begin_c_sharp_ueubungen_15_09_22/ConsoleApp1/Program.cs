5cdfed9a4cb20e0dbae17f2c39627c467c78e9fa
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-15 11:57:28 +0200

    aufgabe 2b #5

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -10,10 +10,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte gebe mir einen Text deiner Wahl");
-            string entry = Console.ReadLine();
-            int length = entry.Length;
-            Console.WriteLine("Dein Text ist " + length + " Zeichen lang");
+            Console.WriteLine("Bitte nenne mir eine Kommazahl");
+
+            double Zahl1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte nenne mir eine zweite Kommazahl");
+            double Zahl2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ich danke dir");
+            double Ergebnis = Zahl1 + Zahl2;
+            Console.WriteLine("Ich kann dir sagen,dass " + Zahl1 + " und " + Zahl2 + " insgesamt " + Ergebnis + " ergibt.");
+
             
 
             Console.ReadLine();
