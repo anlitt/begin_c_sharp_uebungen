854dae1fb8520155dcd9cb8c5f280950bdfd6298
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-15 14:05:19 +0200

    aufgabe 9 issue #10

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace uebungen_c_sharp_150922
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte nenne mir einen Text mit Semicolons");
+            Console.WriteLine("Bitte nenne mir Text");
             string entry = Console.ReadLine();
-            Console.WriteLine("Das ist dein Originaltext: " + entry);
-            string[]teilung = entry.Split(';');
-            foreach (string teil in teilung)
+            string wahr = "abc";
+            bool contains = entry.Contains(wahr);
+            if (contains)
             {
-                Console.WriteLine(teil);
+                Console.WriteLine("true");
             }
+            else
+            {
+                Console.WriteLine("False.Try again");
+            }
+            Console.WriteLine("Danke für deine Teilnahme");
 
+          
 
 
 
