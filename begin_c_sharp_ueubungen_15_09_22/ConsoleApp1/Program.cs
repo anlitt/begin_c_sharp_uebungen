dc208b803826fd6081a7dbe406a6b3f274a69c02
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-15 11:29:55 +0200

    aufgabe #1

---
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahl1 = 12;
+            int Zahl2 = 23;
+                int Ergebnis = Zahl1 + Zahl2;
+            Console.WriteLine(Ergebnis);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
