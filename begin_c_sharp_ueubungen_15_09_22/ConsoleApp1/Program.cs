f90f0dbc7b07f1d2d2ded908520c8a7845e45ed2
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-15 11:50:30 +0200

    aufgabe #4

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -12,10 +12,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Bitte gebe mir einen Text deiner Wahl");
             string entry = Console.ReadLine();
-            string replaced_entry = entry.Replace("U", "G").Replace("u", "g");
-            Console.WriteLine("Dein geänderter Text ist:");
-            Console.WriteLine(replaced_entry);
-
+            int length = entry.Length;
+            Console.WriteLine("Dein Text ist " + length + " Zeichen lang");
             
 
             Console.ReadLine();
