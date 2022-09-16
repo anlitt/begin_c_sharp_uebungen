fcc25e40d18816e4ceaabe4714d0cfb24752c283
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 14:12:49 +0200

    aufgabe 6 #26

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -38,6 +38,8 @@ namespace uebungen_c_sharp_150922
                 
             }
             liste_1.Insert(1, 99);
+            List<int> liste_2 = new List<int>() { 91, 92, 93};
+            liste_1.AddRange(liste_2);
             foreach (int i in liste_1)
             {
                 Console.WriteLine(i);
