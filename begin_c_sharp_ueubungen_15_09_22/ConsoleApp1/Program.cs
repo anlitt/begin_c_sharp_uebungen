cc3e03a697c0df9aca5962758038a2eb0f44a48a
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 13:54:35 +0200

    aufgabe 3 #23

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +16,43 @@ namespace uebungen_c_sharp_150922
         {
             //aufgabe 2
             List<int> liste_1 = new List<int>() { 5,3,7};
+            Console.WriteLine("Möchtest du weitere Zahlen hinzufügen?");
+            int eingabe2 = 0;
+            string eingabe = " ";
+            while(true)
+            {
+                Console.WriteLine("nenne mir eine Zahl, die ich der Liste hinzufügen soll. Wenn keine Zahl hinzugefügt werden soll, drücke Leertaste und dann enter");
+                eingabe = Console.ReadLine();
+                if (eingabe == " ")
+                {
+                    Console.WriteLine("keine weiteren Zahlen werden der Liste hinzugefügt. Abbruch");
+                    break;
+                }
+                else
+                {
+                    eingabe2 = Convert.ToInt32(eingabe);
+                    liste_1.Add(eingabe2);
+                    Console.WriteLine(eingabe2 + " wird deiner Liste hinzugefügt");
 
+                }
+
+            }
+
+
+
+
+            {
+
+            }
+            {
+
+            }
+
+
+            
+            
+            
+           
         }
         
 
