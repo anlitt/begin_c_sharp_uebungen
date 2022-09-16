a3ee202471cfff00e54add6fa18c91bba72dfe71
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 11:55:13 +0200

    taschenrechner mit selbstgeschriebener mehtode #21

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -15,11 +15,59 @@ namespace uebungen_c_sharp_150922
             //rückgabetyp und variablentyp, der gespeichert werden soll, müssen übereinstimmen
             Console.WriteLine("Wir holen uns jetzt eine Zahl aus Number()");
             List<string> names = Number();//methode Number ohne Parameter
-            foreach(var name in names)
+            foreach (var name in names)
             {
-                Console.WriteLine(name);
+                Console.WriteLine("Bitte gib mir eine erste Zahl");
+                int zahl1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bitte gib mir eine zweite Zahl");
+                int zahl2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Was möchtest du machen?");
+                string op = Console.ReadLine();
+                switch (op)
+                {
+                    case "+":
+                        int ergebnis = Add(zahl1, zahl2);
+                        Console.WriteLine("Das Ergebnis ist " + ergebnis);
+                        break;
+                    case "-":
+                        int result = Substraction(zahl1, zahl2);
+                        Console.WriteLine("Das Ergebnis ist " + result);
+                        break;
+                    case "*":
+                        int ergebnis1 = Multiply(zahl1, zahl2);
+                        Console.WriteLine("Das Ergebnis ist " + ergebnis1);
+                        break;
+                    case "/":
+                        int ergebnis2 = Division(zahl1, zahl2);
+                        Console.WriteLine("Das Ergebnis ist " + ergebnis2);
+                        break;
+                }
+
+                
+                
+                Console.ReadLine();
             }
-            Console.ReadLine(); 
+             
+        } 
+        static int Add(int z1, int z2)
+            {
+                int summe = z1 + z2;
+            return summe;
+            }
+        static int Substraction(int z1, int z2)
+        {
+            int differenz = z1 + z2;
+            return differenz;
+        }
+        static int Multiply(int z1, int z2)
+        {
+            int produkt = z1 * z2;
+            return produkt;
+        }
+        static int Division(int z1, int z2)
+        {
+            int quotient = z1/ z2;
+            return quotient;
         }
         //static Rückgabentyp MethodenName (parameter)
         static List<string> Number()
