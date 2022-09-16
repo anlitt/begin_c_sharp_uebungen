caf46f98de6dccb271a6cde115e3f6d5f31bf778
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 13:18:43 +0200

    aufgabe 2 #22

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -12,71 +12,9 @@ namespace uebungen_c_sharp_150922
     {
         static void Main(string[] args)
         {
-            //rückgabetyp und variablentyp, der gespeichert werden soll, müssen übereinstimmen
-            Console.WriteLine("Wir holen uns jetzt eine Zahl aus Number()");
-            List<string> names = Number();//methode Number ohne Parameter
-            foreach (var name in names)
-            {
-                Console.WriteLine("Bitte gib mir eine erste Zahl");
-                int zahl1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Bitte gib mir eine zweite Zahl");
-                int zahl2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Was möchtest du machen?");
-                string op = Console.ReadLine();
-                switch (op)
-                {
-                    case "+":
-                        int ergebnis = Add(zahl1, zahl2);
-                        Console.WriteLine("Das Ergebnis ist " + ergebnis);
-                        break;
-                    case "-":
-                        int result = Substraction(zahl1, zahl2);
-                        Console.WriteLine("Das Ergebnis ist " + result);
-                        break;
-                    case "*":
-                        int ergebnis1 = Multiply(zahl1, zahl2);
-                        Console.WriteLine("Das Ergebnis ist " + ergebnis1);
-                        break;
-                    case "/":
-                        double ergebnis2 = Division(zahl1, zahl2);
-                        Console.WriteLine("Das Ergebnis ist " + ergebnis2);
-                        break;
-                }
+            //aufgabe 2
+            List<int> liste_1 = new List<int>() { 5,3,7};
 
-                
-                
-                Console.ReadLine();
-            }
-             
-        } 
-        static int Add(int z1, int z2)
-            {
-                int summe = z1 + z2;
-            return summe;
-            }
-        static int Substraction(int z1, int z2)
-        {
-            int differenz = z1 + z2;
-            return differenz;
-        }
-        static int Multiply(int z1, int z2)
-        {
-            int produkt = z1 * z2;
-            return produkt;
-        }
-        static double Division(double z1, double z2)
-        {
-            double quotient = z1/ z2;
-            return quotient;
-        }
-        //static Rückgabentyp MethodenName (parameter)
-        static List<string> Number()
-        {
-            List<string> name = new List<string>();
-            name.Add("Abc");
-            //Rückgabevariante
-            return name;
-            //Rückgabewert, Rückgabevariable und Speicherort müssen vom selben Typ sein
         }
         
 
