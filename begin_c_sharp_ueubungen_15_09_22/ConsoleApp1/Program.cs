0aa7a7483a5c613b3d6baa02037e7a116802d654
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-16 11:34:29 +0200

    liste und methods

--- begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
+++ begin_c_sharp_ueubungen_15_09_22/ConsoleApp1/Program.cs
@@ -12,36 +12,25 @@ namespace uebungen_c_sharp_150922
     {
         static void Main(string[] args)
         {
-            List<string> namen = new List<string>();
-            string[] namenArray = new string 
-            
-            Console.WriteLine("wie viele Namen möchtest du eintragen?");
-            int anzahl = Convert.ToInt32(Console.ReadLine());
-            for(int i=0; i<anzahl; i++)
-            {
-                Console.WriteLine("Gib einen Namen ein");
-                namen.Add(Console.ReadLine());
-                
-             
-            }
-            Console.WriteLine("Deine Liste besteht aus folgenden Namen: ");   
-            foreach (string name in namen)
+            //rückgabetyp und variablentyp, der gespeichert werden soll, müssen übereinstimmen
+            Console.WriteLine("Wir holen uns jetzt eine Zahl aus Number()");
+            List<string> names = Number();//methode Number ohne Parameter
+            foreach(var name in names)
             {
                 Console.WriteLine(name);
             }
-            //genauso viele funktionen wie strings
-            //bei listen rutscht es bei removeat auf, und bei array wird es einfach nur leer gemacht aber der platz bleibt bestehen
-            namen.RemoveAt(3);
-            Console.WriteLine("Ich hoffe ich konnte dir helfen");   
-            
-            
-
-
-
-            Console.ReadLine();
+            Console.ReadLine(); 
+        }
+        //static Rückgabentyp MethodenName (parameter)
+        static List<string> Number()
+        {
+            List<string> name = new List<string>();
+            name.Add("Abc");
+            //Rückgabevariante
+            return name;
+            //Rückgabewert, Rückgabevariable und Speicherort müssen vom selben Typ sein
         }
         
-
 
 
     }
